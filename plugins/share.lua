cf9fb92d9cce532135022f5675bcd56f14f1b10b
pouyaxboy <offboy098@gmail.com>
2016-02-26 03:46:43 -0800

    Create share.lua

---
+++ plugins/share.lua
@@ -0,0 +1,16 @@
+do
+
+function run(msg, matches)
+send_contact(get_receiver(msg), "+9809210910812", "MIRROR", "TG", ok_cb, false)
+end
+
+return {
+patterns = {
+"^[!/.](share)$"
+"^(share)$"
+
+},
+run = run
+}
+
+end
