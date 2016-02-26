e3ea02d5d7777ac32f6d5bc486b879fdef674c13
pouyaxboy <offboy098@gmail.com>
2016-02-26 03:03:55 -0800

    Create fosh.lua

---
+++ plugins/fosh.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+  return "از سر تا نوک پا گاییدمت  " .. matches[1]
+end
+
+return {
+    patterns = {
+    "^fosh be (.*)$"
+	"^[!/.](fosh be) (.*)$"
+  }, 
+  run = run 
+}
+
+end
