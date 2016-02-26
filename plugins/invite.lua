2314756c4a09f7a191ef0313bd6d4326439c37b4
pouyaxboy <offboy098@gmail.com>
2016-02-26 04:20:31 -0800

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -1,6 +1,7 @@
 do
 local function callbackres(extra, success, result) -- Callback for res_user in line 27
   local user = 'user#id'..result.id
+  if not is_sudo(msg) then -- For Sudoers only !
 	local chat = 'chat#id'..extra.chatid
 	if is_banned(result.id, extra.chatid) then -- Ignore bans
             send_large_msg(chat, 'User is banned.')
