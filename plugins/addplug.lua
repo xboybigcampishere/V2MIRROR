d482eaf87428fc1b8da335929433ac25ca369e42
pouyaxboy <offboy098@gmail.com>
2016-03-01 07:22:48 -0800

    Update addplug.lua

--- plugins/addplug.lua
+++ plugins/addplug.lua
@@ -1,20 +1,21 @@
- local function run(msg, matches)
- local text = matches[2]
- if matches[1] == "saveplug" and is_sudo(msg) then
- 
-  return "Plugin Saves!!!"
- end
-end
-
-return {
- description = "filemanager plugin saver!",
- usage = {
-  "!saveplug [name][ext] [plugintxt] : save plugin",
- },
- patterns = {
-  "^saveplug ([^%s]+) (.*)$",
-  "^[!/.](saveplug) ([^%s]+) (.*)$",
-  
- },
- run = run,
+local function run(msg, matches)
+if is_sudo(msg) then
+  local text = matches[1]
+  local b = 1
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+  end
+  local name = matches[2]
+  local file = io.open("./plugins/"..name..matches[3], "w")
+  file:write(text)
+  file:flush()
+  file:close()
+  return "done" end return {
+  description = "a Usefull plugin for sudo !",
+  usage = "A plugins to add Another plugins to the server",
+  patterns = {
+    "^addplugin +(.+) (.*) (.*)$"
+  },
+  run = run
 }
